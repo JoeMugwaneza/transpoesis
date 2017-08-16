af96481db176612a402618227c8cfbdf3c376590
JoeMugwaneza <muganezajoseph@gmail.com>
2017-08-16 16:18:50 +0200

    some shit in user model

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170814161258) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.boolean "admin", default: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
