1e6fa5c7948ceeb9e4c7b4e2749bd3b1706c199d
JoeMugwaneza <muganezajoseph@gmail.com>
2017-08-31 21:24:46 +0200

    worked on sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,29 @@
 class SessionsController < ApplicationController
   def new
+   @disable_sidebar = true 
   end
 
   def create
+    @disable_sidebar = true 
     @recent_posts = Post.published.in_order.endmost(5)
+
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+
+    if user.enabled == true && user.authenticate(params[:password])
+
       session[:user_id] = user.id
+      
       redirect_to root_url, notice: 'Logged in!'
+
+    elsif user && user.enabled == false
+
+      flash[:warning] = "Your account has be disabled, contact admin for more info"
+
+       render 'new'
     else 
+
       flash.now.alert = 'Email or password is invalid'
-      render 'new'
+       render 'new'
     end
   end
 
